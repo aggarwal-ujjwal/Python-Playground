ef1cb17ff4098a8773ddde7cdb157e18052cbd48
ujjwal.aggarwal <ujjwal.aggarwal@servicenow.com>
2025-10-09 19:18:07 +0530

    Tips

--- to_do_app/main.py
+++ to_do_app/main.py
@@ -1,3 +1,9 @@
 user_input = input("Enter TO-DO: ")
 #use input argument to print
 
+# first think of naive solution and then optimize it
+# don't start right away and first discuss high level solution
+# Don't go silent, always keep communicating with the interview
+
+
+
